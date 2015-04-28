142b75b8a1212fe006d359a2ecb5af1ed9f3f399
Seth Fitzsimmons <seth@mojodna.net>
2015-04-27 22:08:15 -0700

    Dynamic FP tile server

---
+++ README.md
@@ -0,0 +1,19 @@
+# fp-tiler
+
+I am a tile server for Field Papers atlases and snapshots.
+
+## Usage
+
+```bash
+API_BASE_URL=http://next.fieldpapers.org/ npm start
+```
+
+## Installation
+
+This is a typical NPM-managed install, except that
+[node-gdal](https://github.com/naturalatlas/node-gdal) must be built from
+source in order to support FP's old JPEG-compressed snapshots.
+
+```bash
+npm install --build-from-source=gdal
+```
---
+++ server.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+"use strict";
+
+var path = require("path"),
+    util = require("util");
+
+var express = require("express"),
+    LRU = require("lru-cache"),
+    tessera = require("tessera"),
+    tilelive = require("tilelive-cache")(require("tilelive"));
+
+// TODO use tilelive-modules (see mojodna/tilelive-modules#2)
+require("tilelive-mapnik").registerProtocols(tilelive);
+require("tilelive-raster")(tilelive);
+require("tilelive-fieldpapers")(tilelive);
+
+var API_BASE_URL = process.env.API_BASE_URL || "http://fieldpapers.org/",
+    CACHE = LRU(2),
+    SUPPORT = express();
+
+SUPPORT.use(express.static(path.join(__dirname, "node_modules", "tessera", "public")));
+SUPPORT.use(express.static(path.join(__dirname, "node_modules", "tessera", "bower_components")));
+
+var app = express();
+
+app.use("/:type(snapshots|atlases)/:slug", function(req, res, next) {
+  var key = [req.params.type, req.params.slug].join("-"),
+      route;
+
+  if (!(route = CACHE.get(key))) {
+    route = tessera(tilelive, {
+      source: util.format("fieldpapers+%s%s/%s", API_BASE_URL, req.params.type, req.params.slug)
+    });
+
+    route.use(SUPPORT);
+
+    CACHE.set(key, route);
+  }
+
+  return route(req, res, next);
+});
+
+app.use("/[^\/]+/", SUPPORT);
+
+app.listen(process.env.PORT || 8080, function() {
+  console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+});
